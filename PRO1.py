d9425c74c6d1bbd98d3b103dc5ece07ae6e97ba0
SoumyOp920 <soumymittal09@gmail.com>
2025-04-13 13:58:13 +0530

    Add files via upload

---
+++ PRO1.py
@@ -0,0 +1,10 @@
+a=int(input("Enter 1st number: "))
+b=int(input("Enter 2nd number: "))
+add=a+b
+sub=a-b
+multi=a*b
+div=a/b
+print("Addition: ",add)
+print("Subtraction: ",sub)
+print("Multiplication: ",multi)
+print("Division: ",div)
